a1cd0a4a25cc0de09b9ecd15253cb77a2d7c2b0d
Luis Angel Hernández Leyva <lahernandezl8901@gmail.com>
2019-12-09 11:53:06 -0500

    create tasks module and include into main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/tasks', { useNewUrlParser: true, useFindAndModify: false }),
+    TasksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
