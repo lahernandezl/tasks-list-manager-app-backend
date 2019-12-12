d18966cc9d218a780480012314f3143887bfe453
Luis Angel Hernández Leyva <lahernandezl8901@gmail.com>
2019-12-11 21:43:12 -0500

    creating services to add, get, update and delete tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Task } from './interface/task.interface';
+import { TaskDto } from './dto/task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    constructor(@InjectModel('Task') private readonly taskModel: Model<Task>) { }
+    // fetch all tasks
+    async getAllTasks(): Promise<Task[]> {
+        const task = await this.taskModel.find().exec();
+        return task;
+    }
+    // Get a single task
+    async getTask(taskID: string): Promise<Task> {
+        const task = await this.taskModel.findById(taskID).exec();
+        return task;
+    }
+    // post a single task
+    async addTask(taskDto: TaskDto): Promise<Task> {
+        const newTask = await new this.taskModel(taskDto);
+        return newTask.save();
+    }
+    // Edit task details
+    async updateTask(taskID: string, taskDto: TaskDto): Promise<Task> {
+        const updatedTask = await this.taskModel
+            .findByIdAndUpdate(taskID, TaskDto, { new: true });
+        return updatedTask;
+    }
+    // Delete a task
+    async deleteTask(taskID: string): Promise<any> {
+        const deletedTask = await this.taskModel.findByIdAndRemove(taskID);
+        return deletedTask;
+    }
+}
