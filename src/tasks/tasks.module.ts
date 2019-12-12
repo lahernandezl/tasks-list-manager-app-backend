cba4ef1eeaf068ce3bb80636b70afea73a51d639
Luis Angel Hernández Leyva <lahernandezl8901@gmail.com>
2019-12-11 21:43:58 -0500

    create task controller

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TaskController } from './task.controller';
 import { TaskSchema } from './schema/task.schema';
 import { TasksService } from './tasks.service';
 
 @Module({
   imports: [
       MongooseModule.forFeature([{name: 'Task', schema: TaskSchema}]),
+      TaskController,
   ],
   providers: [
       TasksService,
